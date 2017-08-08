f594abe1210884b2e854748a6a4a2f113b78352c
Branden Soropia <b.soropia@gmail.com>
2017-08-07 22:06:16 -0400

    New layout for adding and subtrating from balance. Saves and reads from shared prefs. Can only subtract from balancce atm.

--- MyFirstAndroidApp/app/src/main/java/com/example/brandensoropia/myfirstandroidapp/MainActivity.java
+++ MyFirstAndroidApp/app/src/main/java/com/example/brandensoropia/myfirstandroidapp/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.brandensoropia.myfirstandroidapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstandroidapp.MESSAGE";
@@ -13,13 +16,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences sharedPref = this.getSharedPreferences(
+                getString(R.string.PREF_FILE_KEY), Context.MODE_PRIVATE);
+
+        float balance = sharedPref.getFloat(getString(R.string.BALANCE), 0.00f);
+        updateBalanceTextView(balance);
     }
 
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.totalCost);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+    private void updateBalanceTextView (float newBalance) {
+        TextView textViewBalance = (TextView) findViewById(R.id.balance);
+        textViewBalance.setText(String.valueOf(newBalance));
+    }
+
+    public void reduceBalance (View view) {
+        EditText editText = (EditText) findViewById(R.id.reduceAmount);
+        float amount = Float.valueOf(editText.getText().toString());
+
+        SharedPreferences sharedPref = this.getSharedPreferences(
+                getString(R.string.PREF_FILE_KEY), Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sharedPref.edit();
+        float balance = sharedPref.getFloat(getString(R.string.BALANCE), 0.00f);
+        float newBalance = balance - amount;
+
+        editor.putFloat(getString(R.string.BALANCE), newBalance);
+        editor.commit();
+        updateBalanceTextView(newBalance);
+    }
+
+    public void goToAddBalance(View view) {
+        Intent intent = new Intent(this, AddBalanceActivity.class);
         startActivity(intent);
     }
 }
