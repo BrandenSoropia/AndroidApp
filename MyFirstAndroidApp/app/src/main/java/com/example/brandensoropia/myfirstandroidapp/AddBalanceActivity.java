bcc36deee777fb19bf7c55769145b6864fba1fa9
Branden Soropia <b.soropia@gmail.com>
2017-08-08 15:25:54 -0400

    Prototype addition and subtraction from balance.

--- MyFirstAndroidApp/app/src/main/java/com/example/brandensoropia/myfirstandroidapp/AddBalanceActivity.java
+++ MyFirstAndroidApp/app/src/main/java/com/example/brandensoropia/myfirstandroidapp/AddBalanceActivity.java
@@ -1,9 +1,12 @@
 package com.example.brandensoropia.myfirstandroidapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class AddBalanceActivity extends AppCompatActivity {
 
@@ -14,7 +17,31 @@ public class AddBalanceActivity extends AppCompatActivity {
     }
 
     public void goToMainActivity (View view) {
+        _gotToMainActivity();
+    }
+
+    public void _gotToMainActivity () {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    public void addBalance (View view) {
+        EditText editText = (EditText) findViewById(R.id.topUpAmount);
+        float amount = Float.valueOf(editText.getText().toString());
+        _addBalance(amount);
+    }
+
+    public void _addBalance (float amount) {
+        SharedPreferences sharedPref = this.getSharedPreferences(
+                getString(R.string.PREF_FILE_KEY), Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sharedPref.edit();
+        float balance = sharedPref.getFloat(getString(R.string.BALANCE), 0.00f);
+        float newBalance = balance + amount;
+
+        editor.putFloat(getString(R.string.BALANCE), newBalance);
+        editor.commit();
+
+        _gotToMainActivity();
     }
 }
